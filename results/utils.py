84b4638727c900bad82210ee336cd3f08a4c2eda
Luciano Bianchi <lucianogbianchi@gmail.com>
2020-02-10 14:57:09 -0300

    Analisis de datos

--- results/utils.py
+++ results/utils.py
@@ -1,13 +1,17 @@
 from scipy.spatial.distance import cdist
 import numpy as np
 
+PATH_OFF = 500
+
 def normal_distance_eval(robot_coords, leader_coords):
     robot_dist_to_leader_path = []
 
-    for r_coord in robot_coords:
-        distances = cdist([r_coord], leader_coords)
+    for i, r_coord in enumerate(robot_coords):
+        from_i = i - PATH_OFF if i > PATH_OFF else 0
+        to_i = i+PATH_OFF
+        distances = cdist([r_coord], leader_coords[from_i:to_i])
         min_dist = np.min(distances)
         robot_dist_to_leader_path.append(min_dist)
         
-    return robot_dist_to_leader_path, np.sum(robot_dist_to_leader_path)
+    return robot_dist_to_leader_path
 
